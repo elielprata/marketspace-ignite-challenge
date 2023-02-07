aae5bfa43b7dca026e2e1a51113428402e99721e
Eliel Martins <elieuprata@gmail.com>
2023-02-07 18:39:53 -0300

    create Button component

---
+++ mobile/src/components/Button.tsx
@@ -0,0 +1,32 @@
+import { Button as NativeBaseButton, IButtonProps, Text } from "native-base";
+
+type Props = IButtonProps & {
+  title: string;
+  variant?: "dark" | "light" | "main";
+};
+
+export function Button({ title, variant = "main", ...rest }: Props) {
+  return (
+    <NativeBaseButton
+      p={3}
+      w="full"
+      bg={
+        variant === "dark"
+          ? "gray.700"
+          : variant === "light"
+          ? "gray.300"
+          : "blue.400"
+      }
+      rounded="lg"
+      {...rest}
+    >
+      <Text
+        fontSize="md"
+        fontWeight="bold"
+        color={variant === "light" ? "gray.600" : "gray.100"}
+      >
+        {title}
+      </Text>
+    </NativeBaseButton>
+  );
+}
