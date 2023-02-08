707b7d175c0e895eff4e413f9c1adbd7f505e272
Eliel Martins <elieuprata@gmail.com>
2023-02-08 09:07:48 -0300

    adjust border width

--- mobile/src/components/UserPhoto.tsx
+++ mobile/src/components/UserPhoto.tsx
@@ -10,7 +10,7 @@ export function UserPhoto({ size, ...rest }: Props) {
       w={size}
       h={size}
       rounded="full"
-      borderWidth={2}
+      borderWidth={3}
       borderColor="blue.400"
       {...rest}
     />
