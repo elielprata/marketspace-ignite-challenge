441cb6f3b9bf60fced10fa9c08ee8715a267f4df
Eliel Martins <elieuprata@gmail.com>
2023-02-09 11:08:24 -0300

    add hStack Props

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,17 +1,23 @@
 import { Button, Heading, HStack, useTheme } from "native-base";
+import { IHStackProps } from "native-base/lib/typescript/components/primitives/Stack/HStack";
 import { ArrowLeft, PencilSimpleLine, Plus } from "phosphor-react-native";
 
-type Props = {
+type Props = IHStackProps & {
   title: string;
   goBack?: boolean;
   rightIcon?: "plus" | "pencil" | null;
 };
 
-export function Header({ title, goBack = true, rightIcon = null }: Props) {
+export function Header({
+  title,
+  goBack = true,
+  rightIcon = null,
+  ...rest
+}: Props) {
   const { colors, sizes } = useTheme();
 
   return (
-    <HStack justifyContent="space-between" alignItems="center">
+    <HStack justifyContent="space-between" alignItems="center" {...rest}>
       <Button w={6} p={0} bg="transparent" _pressed={{ bg: "transparent" }}>
         {goBack && <ArrowLeft color={colors.gray[700]} size={sizes[6]} />}
       </Button>
