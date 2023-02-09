5e529f600ef3e32f6e4e8908033d06e637a702ba
Eliel Martins <elieuprata@gmail.com>
2023-02-09 07:38:37 -0300

    adjust bottom tabs styles

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,18 +1,74 @@
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useTheme } from "native-base";
 
 import { Home } from "@screens/Home";
+import { House, SignOut, Tag } from "phosphor-react-native";
 
 type AuthRoutes = {
   home: undefined;
   adverts: undefined;
+  logout: undefined;
 };
 
 const { Navigator, Screen } = createBottomTabNavigator<AuthRoutes>();
 
 export function AppRoutes() {
+  const { sizes, colors } = useTheme();
+
+  const IconSize = sizes[6];
+
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.gray[600],
+        tabBarInactiveTintColor: colors.gray[400],
+
+        tabBarStyle: {
+          backgroundColor: colors.gray[100],
+          borderTopWidth: 0,
+          paddingBottom: sizes[8],
+          paddingTop: sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <House
+              color={color}
+              size={IconSize}
+              weight={focused ? "bold" : "regular"}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="adverts"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Tag
+              color={color}
+              size={IconSize}
+              weight={focused ? "bold" : "regular"}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="logout"
+        component={Home}
+        options={{
+          tabBarIcon: () => <SignOut color={colors.red[400]} size={IconSize} />,
+        }}
+      />
     </Navigator>
   );
 }
