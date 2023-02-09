fad3768642ef84bcaedd7a27dae0259441f764f1
Eliel Martins <elieuprata@gmail.com>
2023-02-09 12:50:05 -0300

    adjust navigation

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,25 +1,24 @@
 import { Platform } from "react-native";
 import {
-  BottomTabNavigationProp,
-  createBottomTabNavigator,
-} from "@react-navigation/bottom-tabs";
-import { House, SignOut, Tag } from "phosphor-react-native";
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+
 import { useTheme } from "native-base";
+import { House, SignOut, Tag } from "phosphor-react-native";
 
-import { HomeStack } from "./home.routes";
+import { HomeTabs } from "./home.routes";
 
-import { MyAdverts } from "@screens/MyAdverts";
-import { LogOut } from "@screens/LogOut";
+import { AdvertDetails } from "@screens/AdvertDetails";
 
 type AppRoutes = {
   home: undefined;
-  myAdverts: undefined;
-  logout: undefined;
+  advertDetails: undefined;
 };
 
-export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+export type HomeNavigatorRoutesProps = NativeStackNavigationProp<AppRoutes>;
 
-const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+const { Navigator, Screen } = createNativeStackNavigator<AppRoutes>();
 
 export function AppRoutes() {
   const { sizes, colors } = useTheme();
@@ -30,54 +29,11 @@ export function AppRoutes() {
     <Navigator
       screenOptions={{
         headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: colors.gray[600],
-        tabBarInactiveTintColor: colors.gray[400],
-
-        tabBarStyle: {
-          backgroundColor: colors.gray[100],
-          borderTopWidth: 0,
-          height: Platform.OS === "android" ? "auto" : 60,
-          paddingBottom: sizes[8],
-          paddingTop: sizes[6],
-        },
       }}
     >
-      <Screen
-        name="home"
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <House
-              color={color}
-              size={IconSize}
-              weight={focused ? "bold" : "regular"}
-            />
-          ),
-        }}
-      />
+      <Screen name="home" component={HomeTabs} />
 
-      <Screen
-        name="myAdverts"
-        component={MyAdverts}
-        options={{
-          tabBarIcon: ({ color, focused }) => (
-            <Tag
-              color={color}
-              size={IconSize}
-              weight={focused ? "bold" : "regular"}
-            />
-          ),
-        }}
-      />
-
-      <Screen
-        name="logout"
-        component={LogOut}
-        options={{
-          tabBarIcon: () => <SignOut color={colors.red[400]} size={IconSize} />,
-        }}
-      />
+      <Screen name="advertDetails" component={AdvertDetails} />
     </Navigator>
   );
 }
--- mobile/src/routes/home.routes.tsx
+++ mobile/src/routes/home.routes.tsx
@@ -1,24 +1,83 @@
+import { Platform } from "react-native";
 import {
-  createNativeStackNavigator,
-  NativeStackNavigationProp,
-} from "@react-navigation/native-stack";
-import { AdvertDetails } from "@screens/AdvertDetails";
+  BottomTabNavigationProp,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import { useTheme } from "native-base";
+import { House, SignOut, Tag } from "phosphor-react-native";
+
 import { Home } from "@screens/Home";
+import { MyAdverts } from "@screens/MyAdverts";
+import { LogOut } from "@screens/LogOut";
 
 type HomeRoutes = {
-  home: undefined;
+  mainHome: undefined;
+  myAdverts: undefined;
   advertDetails: undefined;
+  logout: undefined;
 };
 
-export type HomeNavigatorRoutesProps = NativeStackNavigationProp<HomeRoutes>;
+export type HomeNavigatorRoutesProps = BottomTabNavigationProp<HomeRoutes>;
 
-const { Navigator, Screen } = createNativeStackNavigator<HomeRoutes>();
+const { Navigator, Screen } = createBottomTabNavigator<HomeRoutes>();
 
-export function HomeStack() {
+export function HomeTabs() {
+  const { sizes, colors } = useTheme();
+
+  const IconSize = sizes[6];
+
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
-      <Screen name="advertDetails" component={AdvertDetails} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.gray[600],
+        tabBarInactiveTintColor: colors.gray[400],
+
+        tabBarStyle: {
+          backgroundColor: colors.gray[100],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 60,
+          paddingBottom: sizes[8],
+          paddingTop: sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="mainHome"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <House
+              color={color}
+              size={IconSize}
+              weight={focused ? "bold" : "regular"}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="myAdverts"
+        component={MyAdverts}
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Tag
+              color={color}
+              size={IconSize}
+              weight={focused ? "bold" : "regular"}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="logout"
+        component={LogOut}
+        options={{
+          tabBarIcon: () => <SignOut color={colors.red[400]} size={IconSize} />,
+        }}
+      />
     </Navigator>
   );
 }
