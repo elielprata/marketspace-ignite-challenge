a5597d72e564e06f82d1d032e058f025a9189933
Eliel Martins <elieuprata@gmail.com>
2023-02-10 17:25:24 -0300

    input show password

--- mobile/src/components/Input.tsx
+++ mobile/src/components/Input.tsx
@@ -1,24 +1,64 @@
-import { IInputProps, Input as NativeBaseInput } from "native-base";
+import {
+  FormControl,
+  HStack,
+  IInputProps,
+  Input as NativeBaseInput,
+  useTheme,
+} from "native-base";
+import { Eye } from "phosphor-react-native";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
-type Props = IInputProps & {};
+type Props = IInputProps & {
+  errorMessage?: string | null;
+  isPassword?: boolean;
+};
 
-export function Input({ ...rest }: Props) {
+export function Input({
+  errorMessage,
+  isPassword = false,
+  isInvalid,
+  ...rest
+}: Props) {
+  const [seePassword, setSeePassword] = useState(false);
+  const invalid = !!errorMessage || isInvalid;
+
+  const { colors, sizes } = useTheme();
+
   return (
-    <NativeBaseInput
-      px={4}
-      pt={3}
-      mb={4}
-      rounded="lg"
-      bg="gray.100"
-      placeholderTextColor="gray.400"
-      borderWidth={0}
-      fontSize="md"
-      color="gray.600"
-      _focus={{
-        bg: "gray.100",
-        borderColor: "gray.500",
-      }}
-      {...rest}
-    />
+    <FormControl isInvalid={invalid} mb={4}>
+      <HStack
+        alignItems="center"
+        rounded="lg"
+        bg="gray.100"
+        overflow="hidden"
+        px={4}
+      >
+        <NativeBaseInput
+          flexShrink={1}
+          placeholderTextColor="gray.400"
+          borderWidth={0}
+          fontSize="md"
+          color="gray.600"
+          _focus={{
+            bg: "gray.100",
+            borderColor: "gray.500",
+          }}
+          secureTextEntry={!seePassword}
+          {...rest}
+        />
+
+        {isPassword && (
+          <TouchableOpacity onPress={() => setSeePassword(!seePassword)}>
+            <Eye
+              color={colors.gray[500]}
+              size={sizes[5]}
+              weight={seePassword ? "bold" : "regular"}
+            />
+          </TouchableOpacity>
+        )}
+      </HStack>
+      <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
   );
 }
