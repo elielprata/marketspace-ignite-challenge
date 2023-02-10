dc8f028326e105a2dfe6c1475590a91082a2b6f6
Eliel Martins <elieuprata@gmail.com>
2023-02-10 11:24:19 -0300

    remove white background on navigation

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Box } from "native-base";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.200">
+      <NavigationContainer>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
