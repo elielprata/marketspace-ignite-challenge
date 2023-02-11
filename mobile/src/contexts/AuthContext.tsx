f9e20256d9edd93524cefa19bedcf93952b39d43
Eliel Martins <elieuprata@gmail.com>
2023-02-11 08:16:46 -0300

    create singIn function on context

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,9 +1,11 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 
 export type AuthContextDataProps = {
   user: UserDTO;
+  signIn: (email: string, password: string) => void;
 };
 
 type AuthContextProviderProps = {
@@ -15,18 +17,22 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { email, password });
+
+      if (data.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
   return (
-    <AuthContext.Provider
-      value={{
-        user: {
-          name: "elieu",
-          email: "elieu",
-          avatar: "eliel",
-          id: "1",
-          tel: "",
-        },
-      }}
-    >
+    <AuthContext.Provider value={{ user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
