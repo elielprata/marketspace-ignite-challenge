26e30d2326b63afc24bb8a32ce5fdd1eebae7a56
Eliel Martins <elieuprata@gmail.com>
2023-02-11 08:40:18 -0300

    use component loading while load user Data

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -5,13 +5,18 @@ import { useAuth } from "@hooks/useAuth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
+import { Loading } from "@components/Loading";
 
 export function Routes() {
   const { colors } = useTheme();
-  const { user } = useAuth();
+  const { user, isLoadingUserData } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[200];
+
+  if (isLoadingUserData) {
+    return <Loading />;
+  }
 
   return (
     <Box flex={1} bg="gray.200">
