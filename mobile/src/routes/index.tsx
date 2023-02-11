226a30e1b101435bcd6c4b6fa8b74521ac1bd1fa
Eliel Martins <elieuprata@gmail.com>
2023-02-11 08:16:19 -0300

    verify is user isLogged and select routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,14 +1,22 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { Box } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { Box, useTheme } from "native-base";
+
+import { useAuth } from "@hooks/useAuth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { colors } = useTheme();
+  const { user } = useAuth();
+
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[200];
+
   return (
     <Box flex={1} bg="gray.200">
-      <NavigationContainer>
-        <AuthRoutes />
+      <NavigationContainer theme={theme}>
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
