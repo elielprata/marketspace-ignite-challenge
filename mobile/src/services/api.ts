b8d5183f500543eb6d12a2ad82d1f9fbbba19497
Eliel Martins <elieuprata@gmail.com>
2023-02-11 06:57:54 -0300

    add interceptor to show customized message

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,21 @@
+import { AppError } from "@utils/AppError";
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "http://192.168.1.108:3000",
+const api = axios.create({
+  baseURL: "http://192.168.1.108:3333",
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(
+        new AppError("Erro no servidor. Tente novamente mais tarde.")
+      );
+    }
+  }
+);
+
+export { api };
