97a94b846172c35d84d45687a3161e1e7dcc2ed1
Eliel Martins <elieuprata@gmail.com>
2023-02-11 15:20:57 -0300

    adjust home screen Header component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -20,41 +20,44 @@ export function Home() {
 
   return (
     <VStack flex={1} bg="gray.200" px={6} pt={16}>
-      <HomeHeader />
-
-      <VStack my={8}>
-        <Text fontSize="sm" mb={3}>
-          Seus produtos anunciados para venda
-        </Text>
-
-        <HStack px={4} py={3} bg="blue.700:alpha.10" alignItems="center">
-          <Tag color={colors.blue["700"]} size={sizes["6"]} />
-
-          <VStack flex={1} ml={3}>
-            <Heading>0</Heading>
-            <Text>anúncios ativos</Text>
-          </VStack>
-
-          <HStack>
-            <Text color="blue.700" fontSize="sm" fontWeight="bold" mr={2}>
-              Meus anúncios
-            </Text>
-            <ArrowRight color={colors.blue["700"]} size={sizes["5"]} />
-          </HStack>
-        </HStack>
-      </VStack>
-
-      <VStack mb={6}>
-        <Text fontSize="sm" mb={3}>
-          Compre produtos variados
-        </Text>
-
-        <SearchInput />
-      </VStack>
-
       <FlatList
         data={adverts}
         keyExtractor={(item: number) => `${item}`}
+        ListHeaderComponent={() => (
+          <>
+            <HomeHeader />
+
+            <VStack my={8}>
+              <Text fontSize="sm" mb={3}>
+                Seus produtos anunciados para venda
+              </Text>
+
+              <HStack px={4} py={3} bg="blue.700:alpha.10" alignItems="center">
+                <Tag color={colors.blue["700"]} size={sizes["6"]} />
+
+                <VStack flex={1} ml={3}>
+                  <Heading>0</Heading>
+                  <Text>anúncios ativos</Text>
+                </VStack>
+
+                <HStack>
+                  <Text color="blue.700" fontSize="sm" fontWeight="bold" mr={2}>
+                    Meus anúncios
+                  </Text>
+                  <ArrowRight color={colors.blue["700"]} size={sizes["5"]} />
+                </HStack>
+              </HStack>
+            </VStack>
+
+            <VStack mb={6}>
+              <Text fontSize="sm" mb={3}>
+                Compre produtos variados
+              </Text>
+
+              <SearchInput />
+            </VStack>
+          </>
+        )}
         renderItem={() => (
           <AdvertCard condition="new" onPress={handleOpenAdvertDetails} />
         )}
