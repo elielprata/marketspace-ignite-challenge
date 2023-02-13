7f15a7c009bd620c944033539a67fb7e095d6118
Eliel Martins <elieuprata@gmail.com>
2023-02-13 09:36:09 -0300

    adjust alignment and add call to onRemove method

--- mobile/src/components/ProductPhoto.tsx
+++ mobile/src/components/ProductPhoto.tsx
@@ -1,18 +1,40 @@
-import { IImageProps, Image } from "native-base";
+import {
+  Box,
+  Button,
+  IImageProps,
+  Image,
+  Pressable,
+  useTheme,
+} from "native-base";
+import { X } from "phosphor-react-native";
+import { TouchableOpacity } from "react-native";
 
 type Props = IImageProps & {
   size: number;
+  onRemove: () => void;
 };
 
-export function ProductPhoto({ size, ...rest }: Props) {
+export function ProductPhoto({ size, onRemove, ...rest }: Props) {
+  const { colors, sizes } = useTheme();
   return (
-    <Image
-      w={size}
-      h={size}
-      rounded="md"
-      mr={2}
-      alt="Imagem do produto"
-      {...rest}
-    />
+    <Box mr={2}>
+      <Image w={size} h={size} rounded="md" alt="Imagem do produto" {...rest} />
+
+      <Pressable
+        position="absolute"
+        top={1}
+        right={1}
+        bg="gray.600"
+        w={4}
+        h={4}
+        justifyContent="center"
+        alignItems="center"
+        rounded="full"
+        _pressed={{ bg: "gray.500" }}
+        onPress={onRemove}
+      >
+        <X color={colors.gray[100]} size={sizes[3]} />
+      </Pressable>
+    </Box>
   );
 }
