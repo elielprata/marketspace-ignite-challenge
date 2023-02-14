ec46f06484aa5d929513a39fe276fac7fbe52994
Eliel Martins <elieuprata@gmail.com>
2023-02-13 22:11:14 -0300

    update ProductDTO

--- mobile/src/dtos/ProductDTO.ts
+++ mobile/src/dtos/ProductDTO.ts
@@ -2,6 +2,9 @@ export type ProductDTO = {
   id?: string;
   name: string;
   description: string;
+  user: {
+    avatar: string;
+  };
   is_new: boolean;
   price: number;
   accept_trade: boolean;
