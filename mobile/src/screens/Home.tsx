75b9cb56573f9af28219e00f29eff627dc7b94cd
Eliel Martins <elieuprata@gmail.com>
2023-02-13 22:12:38 -0300

    fetch products of others users

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,27 +1,65 @@
-import { useState } from "react";
-import { FlatList, Heading, HStack, Text, useTheme, VStack } from "native-base";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Heading,
+  HStack,
+  Text,
+  useTheme,
+  useToast,
+  VStack,
+} from "native-base";
 import { ArrowRight, Tag } from "phosphor-react-native";
+
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
+
+import { useNavigation } from "@react-navigation/native";
+import { HomeNavigatorRoutesProps } from "@routes/home.routes";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { SearchInput } from "@components/SearchInput";
 import { AdvertCard } from "@components/AdvertCard";
-import { useNavigation } from "@react-navigation/native";
-import { HomeNavigatorRoutesProps } from "@routes/home.routes";
+import { ProductDTO } from "@dtos/ProductDTO";
 
 export function Home() {
-  const [adverts, setAdverts] = useState([1, 2, 3, 4, 5, 6, 7]);
+  const [products, setProducts] = useState<{}[]>([]);
+
+  const toast = useToast();
 
   const { colors, sizes } = useTheme();
   const navigation = useNavigation<HomeNavigatorRoutesProps>();
 
-  function handleOpenAdvertDetails() {
-    navigation.navigate("advertDetails");
+  function handleOpenAdvertDetails(productId: string) {
+    navigation.navigate("advertDetails", { productId });
   }
+
+  async function fetchProducts() {
+    try {
+      const response = await api.get("/products");
+
+      setProducts(response.data as ProductDTO[]);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os produtos";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <VStack flex={1} bg="gray.200" px={6} pt={16}>
       <FlatList
-        data={adverts}
+        data={products}
         keyExtractor={(item: number) => `${item}`}
         ListHeaderComponent={() => (
           <>
@@ -58,8 +96,12 @@ export function Home() {
             </VStack>
           </>
         )}
-        renderItem={() => (
-          <AdvertCard condition="new" onPress={handleOpenAdvertDetails} />
+        renderItem={({ item }) => (
+          <AdvertCard
+            userPhoto={item.user.avatar}
+            data={item}
+            onPress={() => handleOpenAdvertDetails(item.id)}
+          />
         )}
         flex={1}
         showsVerticalScrollIndicator={false}
